ed61ec3e3249cd4b31f422825c99baa33d60711b
comatthewb <co.matthewb@gmail.com>
2020-03-28 20:21:50 -0500

    added second react render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,3 +3,4 @@ import ReactDOM from "react-dom";
 import RelatedItems from "./App";
 
 ReactDOM.render(<RelatedItems />, document.getElementById("related"));
+ReactDOM.render(<RelatedItems />, document.getElementById("related2"));
